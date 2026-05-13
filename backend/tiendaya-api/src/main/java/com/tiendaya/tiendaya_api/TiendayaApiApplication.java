103e8815d28b01a7de380158198670115ca319f2
Sam-J-R <samuelitojarro@gmail.com>
2026-05-13 11:23:37 -0400

    Creacion directorios de carptes y spring para backend
    Se dividio por directorios a backen, fornten y documentos, se creo spring para la API con las dependencias requeridas para backend

---
+++ backend/tiendaya-api/src/main/java/com/tiendaya/tiendaya_api/TiendayaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaya.tiendaya_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendayaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendayaApiApplication.class, args);
+	}
+
+}
---
+++ backend/tiendaya-api/src/test/java/com/tiendaya/tiendaya_api/TiendayaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaya.tiendaya_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendayaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
